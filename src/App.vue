0b2a1ec817ffd5ad9f0da4ae5c51068fc74825d9
StefanoGalandrini <stefe68@gmail.com>
2023-07-14 14:13:42 +0200

    Init repo - vite installed

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import ProjectList from "./components/ProjectList.vue";
+
+export default {
+	components: {
+		ProjectList,
+	},
+};
+</script>
+
+<template>
+	<h1>Boolpress</h1>
+	<ProjectList />
+</template>
+
+<style lang="scss" scoped>
+@import "bootstrap/scss/bootstrap";
+</style>
---
+++ src/components/ProjectList.vue
@@ -0,0 +1,41 @@
+<script>
+import axios from "axios";
+
+export default {
+	data() {
+		return {
+			arrProjects: [],
+			nPages: 0,
+		};
+	},
+
+	// methods: {
+	// 	getProjects()
+	// 	{
+
+	// }
+
+	created() {
+		axios
+			.get("http://localhost:8000/api/projects", {
+				params: this.nPage,
+			})
+			.then((response) => {
+				this.arrProjects = response.data.data;
+				this.nPages;
+			});
+	},
+};
+</script>
+
+<template>
+	<h2>I nostri progetti</h2>
+
+	<ul>
+		<li v-for="project in arrProjects" :key="project.id">
+			{{ project.title }}
+		</li>
+	</ul>
+</template>
+
+<style lang="scss"></style>
